60c6220f9ddca99aef9899879cb5c1492874964b
Ethan Ratnofsky <43038541+ethanratnofsky@users.noreply.github.com>
2022-11-11 21:06:15 -0600

    create template for demo data

---
+++ frontend/demoApartmentData.js
@@ -0,0 +1,17 @@
+const APARTMENTS = {
+    "Aertson Midtown": [
+        {
+            sqaureFeet: 2300,
+            numBeds: 2,
+            numBaths: 2,
+            startingPrice: 2000,
+        }
+    ],
+    "Kinect": [],
+    "Artemis Midtown": [],
+    "Apollo Midtown": [],
+    "Elliston 23": [],
+    "Acklen West End": [],
+};
+
+export default APARTMENTS;
