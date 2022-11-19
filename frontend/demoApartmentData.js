0825ecfdcb35b5beac09d60cff52c5e96e09fbfd
ehan7 <han.ethan.07@gmail.com>
2022-11-18 20:48:47 -0600

    apartment data

--- frontend/demoApartmentData.js
+++ frontend/demoApartmentData.js
@@ -1,17 +1,124 @@
 const APARTMENTS = {
     "Aertson Midtown": [
         {
-            sqaureFeet: 2300,
+            squareFeet: 2300,
             numBeds: 2,
             numBaths: 2,
             startingPrice: 2000,
+        },
+        {
+            squareFeet: 662,
+            numBeds: 1,
+            numBaths: 1,
+            startingPrice: 1725,
+        },
+        {
+            squareFeet: 825,
+            numBeds: 1,
+            numBaths: 1,
+            startingPrice: 2091,
         }
     ],
-    "Kinect": [],
-    "Artemis Midtown": [],
-    "Apollo Midtown": [],
-    "Elliston 23": [],
-    "Acklen West End": [],
+    "Kenect": [
+        {
+            squareFeet: 1110,
+            numBeds: 3,
+            numBaths: 3,
+            startingPrice: 1468,
+        },
+        {
+            squareFeet: 551,
+            numBeds: 1,
+            numBaths: 1,
+            startingPrice: 2500,
+        },
+        {
+            squareFeet: 360,
+            numBeds: 0,
+            numBaths: 1,
+            startingPrice: 1822,
+        }
+    ],
+    "Artemis Midtown": [
+        {
+            squareFeet: 504,
+            numBeds: 1,
+            numBaths: 1,
+            startingPrice: 1399,
+        },
+        {
+            squareFeet: 1038,
+            numBeds: 2,
+            numBaths: 2,
+            startingPrice: 2500,
+        },
+        {
+            squareFeet: 662,
+            numBeds: 0,
+            numBaths: 1,
+            startingPrice: 1300,
+        }
+    ],
+    "Apollo Midtown": [
+        {
+            squareFeet: 663,
+            numBeds: 1,
+            numBaths: 1,
+            startingPrice: 1695,
+        },
+        {
+            squareFeet: 968,
+            numBeds: 2,
+            numBaths: 2,
+            startingPrice: 2588,
+        },
+        {
+            squareFeet: 1090,
+            numBeds: 2,
+            numBaths: 2,
+            startingPrice: 2740,
+        }
+    ],
+    "Elliston 23": [
+        {
+            squareFeet: 692,
+            numBeds: 1,
+            numBaths: 1,
+            startingPrice: 1800,
+        },
+        {
+            squareFeet: 1079,
+            numBeds: 2,
+            numBaths: 2,
+            startingPrice: 2600,
+        },
+        {
+            squareFeet: 1219,
+            numBeds: 2,
+            numBaths: 2,
+            startingPrice: 2700,
+        }
+    ],
+    "Acklen West End": [
+        {
+            squareFeet: 655,
+            numBeds: 1,
+            numBaths: 1,
+            startingPrice: 1703,
+        },
+        {
+            squareFeet: 963,
+            numBeds: 2,
+            numBaths: 2,
+            startingPrice: 2448,
+        },
+        {
+            squareFeet: 963,
+            numBeds: 2,
+            numBaths: 2,
+            startingPrice: 2458,
+        }
+    ],
 };
 
 export default APARTMENTS;
