1f15d187ab2c38ed7134362ba2dc776765f86bfc
ehan7 <han.ethan.07@gmail.com>
2022-11-22 15:59:53 -0500

    some changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,9 +7,17 @@ import "./App.css";
 // The main React App component
 const App = () => {
     const [searchInput, setSearchInput] = useState("");
+    const [minPriceInput, setMinPriceInput] = useState(0);
+    const [maxPriceInput, setMaxPriceInput] = useState(0);
     
     const handleSearchInputChange = (event) => {
         setSearchInput(event.target.value);
+    }
+    const handleMinPriceInputChange = (event) => {
+        setMinPriceInput(parseInt(event.target.value));
+    }
+    const handleMaxPriceInputChange = (event) => {
+        setMaxPriceInput(parseInt(event.target.value));
     }
 
     const handleSearch = () => {
@@ -17,7 +25,7 @@ const App = () => {
     }
 
     return (
-        <>
+        <div className="container">
             <h1>House Vandy</h1>
             <h4>Built for Vandy students by Vandy Students</h4>
             <div className="search-container">
@@ -25,12 +33,20 @@ const App = () => {
                 <button onClick={handleSearch}>Search</button>
             </div>
             <div className="filters-container">
+                <label htmlFor="min-price">
+                    Min Price
+                </label>
+                <input type="number" name="min-price" id="min-price" placeholder="Enter minimum price" value={minPriceInput} onChange={handleMinPriceInputChange} />
 
+                <label htmlFor="max-price">
+                    Max Price
+                </label>
+                <input type="number" name="max-price" id="max-price" placeholder="Enter maximum price" value={maxPriceInput} onChange={handleMaxPriceInputChange} /> 
             </div>
             <div className="search-results">
                 
             </div>
-        </>
+        </div>
     );
 };
 
