497eae9f5d01a0b38573f325e829474f42537d79
Ethan Ratnofsky <43038541+ethanratnofsky@users.noreply.github.com>
2022-11-22 15:32:02 -0500

    Preliminary frontend content

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,36 @@
 // Packages
-import React from "react";
+import React, { useState } from "react";
 
 // Styles
 import "./App.css";
 
 // The main React App component
-const App = () =>{
+const App = () => {
+    const [searchInput, setSearchInput] = useState("");
+    
+    const handleSearchInputChange = (event) => {
+        setSearchInput(event.target.value);
+    }
+
+    const handleSearch = () => {
+        alert("You searched for: " + searchInput);
+    }
+
     return (
-        <h1>
-            House Vandy 
-        </h1>
+        <>
+            <h1>House Vandy</h1>
+            <h4>Built for Vandy students by Vandy Students</h4>
+            <div className="search-container">
+                <input type="search" id="search-bar" placeholder="Search" value={searchInput} onChange={handleSearchInputChange} />
+                <button onClick={handleSearch}>Search</button>
+            </div>
+            <div className="filters-container">
+
+            </div>
+            <div className="search-results">
+                
+            </div>
+        </>
     );
 };
 
