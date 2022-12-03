03f6913c55055d3205d4a706c712c603ff906ab0
Ethan Ratnofsky <43038541+ethanratnofsky@users.noreply.github.com>
2022-12-02 23:03:54 -0600

    Add CORs

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 3000;
 
 const scraper = require('./routes/scraper.router');
 
+app.use(cors());
 
 app.use('/scraper', scraper);
 
